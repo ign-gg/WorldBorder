7e8478dee0c30ffb516ff580a94f46e4343431bf
Petteri <petteri.malkavaara1@gmail.com>
2020-12-26 17:47:29 +0200

    Nether coords

--- src/main/java/idk/plugin/worldborder/Main.java
+++ src/main/java/idk/plugin/worldborder/Main.java
@@ -1,17 +1,19 @@
 package idk.plugin.worldborder;
 
 import cn.nukkit.Player;
-import cn.nukkit.event.player.PlayerTeleportEvent;
-import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerMoveEvent;
+import cn.nukkit.event.player.PlayerTeleportEvent;
+import cn.nukkit.level.Level;
 import cn.nukkit.math.Vector3;
+import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 
 public class Main extends PluginBase implements Listener {
     
     private int distance;
+    private int netherDistance;
     private String message;
     private String messageTp;
     private boolean teleportToSpawn;
@@ -30,6 +32,7 @@ public class Main extends PluginBase implements Listener {
         }
 
         distance = getConfig().getInt("distance");
+        netherDistance = distance >> 3;
         message = getConfig().getString("message");
         messageTp = getConfig().getString("messageTp");
         teleportToSpawn = getConfig().getBoolean("teleportToSpawn");
@@ -38,7 +41,7 @@ public class Main extends PluginBase implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
-        if (p.distance(new Vector3(p.getLevel().getSpawnLocation().getX(), p.getY(), p.getLevel().getSpawnLocation().getZ())) > distance) {
+        if (p.distance(new Vector3(p.getLevel().getSpawnLocation().getX(), p.getY(), p.getLevel().getSpawnLocation().getZ())) > (p.getLevel().getDimension() == Level.DIMENSION_NETHER ? netherDistance : distance)) {
             p.sendMessage(message);
             e.setCancelled(true);
 
